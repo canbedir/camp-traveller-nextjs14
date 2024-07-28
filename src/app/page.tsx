9bf412f1d0cfd2bbb3f2c823c0136b5ebc0a3287
hix <canbedir100@gmail.com>
2024-07-28 19:46:35 +0300

    Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,23 @@
-"use client"
+"use client";
 import { useEffect } from "react";
-import 'aos/dist/aos.css'
-import AOS from "aos"
+import "aos/dist/aos.css";
+import AOS from "aos";
 import { Button } from "@/components/ui/button";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
-
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init({
       offset: 200,
       duration: 600,
       easing: "ease-in-sine",
       delay: 100,
-    })
-  }, [])
+    });
+  }, []);
 
   return (
-    <>
-
-      <div data-aos="fade-down" data-aos-once="true" data-aos-delay="500">
-        <h1>Hello world</h1>
-      </div>
-
-      
-
-    </>
+    <div className="font-roboto">
+      <Navbar />
+    </div>
   );
 }
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+import { NAV_LINKS } from "@/constans";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { Menu, MenuSquare, User } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <nav className="border border-b shadow-sm ">
+      <div className="flex justify-between items-center px-3 py-3 relative z-30 container">
+        <Link
+          href={"/"}
+          className="font-semibold text-xl"
+          data-aos="fade-left"
+          data-aos-once="true"
+          data-aos-delay="100"
+        >
+          <span className="text-orange-500">Camp</span> <span>Traveller</span>
+        </Link>
+
+        <ul className="hidden lg:flex h-full gap-10">
+          {NAV_LINKS.map((link) => (
+            <Link
+              key={link.key}
+              href={link.href}
+              className="font-poppins flex items-center cursor-pointer"
+              data-aos="fade-down"
+              data-aos-once="true"
+              data-aos-delay={link.delay}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+
+        <div className="hidden lg:flex justify-center">
+          <Button
+            type="button"
+            className="flex gap-1.5"
+            data-aos="fade-left"
+            data-aos-once="true"
+            data-aos-delay="1100"
+          >
+            <User className="h-4 w-4" /> Login
+          </Button>
+        </div>
+
+        <Menu className="lg:hidden inline-block cursor-pointer w-8 h-8 text-orange-500" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
